da0199d9b83788466acd8da13cd30d374a4aeb5e
Gal <128787803+gal-s2@users.noreply.github.com>
2025-03-05 17:01:27 +0200

    Docker (#3)

---
+++ Server/config/mongo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+//const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhose:27017/';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://mongo:27017/';
+
+const connectMongo = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Connected to Mongo successfully.');
+    } catch(error) {
+        console.log('MongoDB Connection Error:', error);
+        process.exit(1);
+    }
+}
+
+connectMongo();
