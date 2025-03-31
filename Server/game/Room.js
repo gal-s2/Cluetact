e63f40f58235b595821dad67b4a4fff5171ebec1
idomaori98 <idoma@mta.ac.il>
2025-03-31 12:44:01 +0300

    connection between player and user

--- Server/game/Room.js
+++ Server/game/Room.js
@@ -2,13 +2,24 @@ const Player = require('./Player');
 const GameSession = require('./GameSession')
 
 class Room {
-    constructor(roomId, status, keeperId, listOfSeekersIds) {
+    constructor(roomId, status, keeperId, listOfSeekersIds, usernamesMap = {}) {
         this.roomId = roomId;
-        this.status = status;
+        this.status = status; // Created, inProgress, Ended
         this.keeperId = keeperId; 
         this.listOfSeekersIds = listOfSeekersIds;
-
         this.currentSession = new GameSession();
+
+         // Add keeper
+         const keeper = new Player(keeperId, usernamesMap[keeperId] || 'Unknown');
+         keeper.setRole('keeper');
+         this.players[keeperId] = keeper;
+ 
+         // Add seekers
+         listOfSeekersIds.forEach((id) => {
+             const seeker = new Player(id, usernamesMap[id] || 'Unknown');
+             seeker.setRole('seeker');
+             this.players[id] = seeker;
+         });
     }
 
     updateStatus(status) {
