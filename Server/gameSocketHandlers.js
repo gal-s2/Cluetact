13d877db1a666954fc99da47d196226053f1b531
galsu <128787803+gal-s2@users.noreply.github.com>
2025-04-15 00:36:52 +0300

    Sockets logger

--- Server/gameSocketHandlers.js
+++ Server/gameSocketHandlers.js
@@ -1,14 +1,18 @@
 const GameFactory = require('./game/GameFactory');
-const Logger = require('./game/Logger.js');
+const { socketLogger } = require('./logger.js');
 
 module.exports = function(io) {
   const game = new GameFactory();
 
   io.on('connection', (socket) => {
+    socket.onAny((event, ...args) => {
+      socketLogger.info(`[Socket ${socket.id}] Event: ${event} | Data: ${JSON.stringify(args)}`);
+    });
+
     console.log('Client connected:', socket.id);
 
     socket.on('join_game', ({ userId, username }) => {
-      console.log(` Player joined: ${username} (${userId})`);
+      //console.log(` Player joined: ${username} (${userId})`);
       game.addUserToQueue(userId);
       io.emit('player_joined', { username });
     });
--- Server/index.js
+++ Server/index.js
@@ -7,32 +7,31 @@ require('dotenv').config();
 //require('./config/redis');
 require('./config/mongo');
 
-const http = require('http');
-const { Server } = require('socket.io');
+const PORT = process.env.PORT || 8000;
 const app = express();
 const cors = require('cors');
-
 const { requestLogger } = require("./logger");
 
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    requestLogger.info(`${req.method} ${req.url}`);
-    next();
-});
+  requestLogger.info(`${req.method} ${req.url}`);
+  next();
+})
 
+// Import all routers from ./routes/index.js
+app.use(require('./routes'));
+
+const http = require('http');
+const { Server } = require('socket.io');
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] }
 });
 
 require('./gameSocketHandlers.js')(io);
 
-// Import all routers from ./routes/index.js
-app.use(require('./routes'));
-
-
-const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
