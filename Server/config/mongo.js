273a651212761e20ab8378f6403da8d14528d838
idomaori98 <idoma@mta.ac.il>
2025-04-23 19:43:17 +0300

    update mongo location in mongodb

--- Server/config/mongo.js
+++ Server/config/mongo.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/Cluetact";
 //const MONGO_URI = process.env.MONGO_URI || 'mongodb://mongo:27017/';
 
 const connectMongo = async () => {
     try {
         await mongoose.connect(MONGO_URI);
-        console.log('Connected to Mongo successfully.');
-    } catch(error) {
-        console.log('MongoDB Connection Error:', error);
+        console.log("Connected to Mongo successfully.");
+    } catch (error) {
+        console.log("MongoDB Connection Error:", error);
         process.exit(1);
     }
-}
+};
 
 connectMongo();
