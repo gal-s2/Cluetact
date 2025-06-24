93bd672ad6ccd987367e22cf5f9f6105ffd6193d
Daniel Omesi <omesidaniel@gmail.com>
2025-06-24 08:18:49 +0300

    updated server-url

--- Client/src/config/baseUrl.js
+++ Client/src/config/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8000";
+export const baseUrl = import.meta.env.VITE_SERVER_URL;
 //"http://localhost:8000";
 //"https://cluetact-server.onrender.com"
 //#server-url
