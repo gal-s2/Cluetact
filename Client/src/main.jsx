241d3508c2cd8c903dbb3d83cf260c8af8db9205
Daniel Omesi <omesidaniel@gmail.com>
2025-06-25 17:25:15 +0300

    added google authentication

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { UserProvider } from "./contexts/UserContext.jsx";
 import { GlobalNotificationProvider } from "./contexts/GlobalNotificationContext.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <GlobalNotificationProvider>
-            <UserProvider>
-                <App />
-            </UserProvider>
-        </GlobalNotificationProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+            <GlobalNotificationProvider>
+                <UserProvider>
+                    <App />
+                </UserProvider>
+            </GlobalNotificationProvider>
+        </GoogleOAuthProvider>
     </StrictMode>
 );
