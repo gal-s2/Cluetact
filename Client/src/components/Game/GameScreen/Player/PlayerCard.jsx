c8c9717601f0fb3d283b7555e7244eff8c6c785a
galsu <128787803+gal-s2@users.noreply.github.com>
2025-08-03 22:39:24 +0300

    Game Profile

--- Client/src/components/Game/GameScreen/Player/PlayerCard.jsx
+++ Client/src/components/Game/GameScreen/Player/PlayerCard.jsx
@@ -2,40 +2,74 @@ import classNames from "classnames";
 import styles from "./PlayerCard.module.css";
 import { avatarList } from "@utils/loadAvatars";
 
-const PlayerCard = ({ player, me, isActiveClueGiver, onClick, setSelectedPlayer }) => {
+const PlayerCard = ({ player, me, isActiveClueGiver, selectedPlayer, setSelectedPlayer }) => {
     const avatarSrc = avatarList[player.avatar] || avatarList[0];
+    const isExpanded = selectedPlayer?.username === player.username;
+
+    const handleCardClick = () => {
+        if (isExpanded) {
+            setSelectedPlayer(null); // Close if already open
+        } else {
+            setSelectedPlayer(player);
+        }
+    };
 
     return (
-        <div
-            className={classNames(styles.card, {
-                [styles.me]: me,
-                [styles.activeClueGiver]: isActiveClueGiver,
-                [styles.keeper]: player.role === "keeper",
-                [styles.seeker]: player.role === "seeker",
-            })}
-            onClick={() => {
-                onClick(player);
-                setSelectedPlayer(player);
-            }}
-        >
-            <div className={styles.inner}>
-                <div className={styles.playerImageContainer}>
-                    <img className={styles.playerImage} src={avatarSrc} alt={`${player.username}'s avatar`} />
-                    {isActiveClueGiver && (
-                        <div className={styles.turnIndicator}>
-                            <span className={styles.turnDot}></span>
+        <div className={styles.cardWrapper}>
+            <div
+                className={classNames(styles.card, {
+                    [styles.me]: me,
+                    [styles.activeClueGiver]: isActiveClueGiver,
+                    [styles.keeper]: player.role === "keeper",
+                    [styles.seeker]: player.role === "seeker",
+                    [styles.expanded]: isExpanded,
+                })}
+                onClick={handleCardClick}
+            >
+                <div className={styles.inner}>
+                    <div className={styles.playerImageContainer}>
+                        <img className={styles.playerImage} src={avatarSrc} alt={`${player.username}'s avatar`} />
+                        {isActiveClueGiver && (
+                            <div className={styles.turnIndicator}>
+                                <span className={styles.turnDot}></span>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.playerDataContainer}>
+                        <h3>{player.username}</h3>
+                        <p>{player.gameScore} pts</p>
+                        <div className={styles.badgeContainer}>
+                            {me && <span className={styles.youTag}>YOU</span>}
+                            {isActiveClueGiver && <span className={styles.turnTag}>CURRENT TURN</span>}
                         </div>
-                    )}
-                </div>
-                <div className={styles.playerDataContainer}>
-                    <h3>{player.username}</h3>
-                    <p>{player.gameScore} pts</p>
-                    <div className={styles.badgeContainer}>
-                        {me && <span className={styles.youTag}>YOU</span>}
-                        {isActiveClueGiver && <span className={styles.turnTag}>CURRENT TURN</span>}
+                    </div>
+                    <div className={styles.expandIcon}>
+                        <span className={classNames(styles.chevron, { [styles.rotated]: isExpanded })}>▼</span>
                     </div>
                 </div>
             </div>
+
+            {/* Dropdown Profile */}
+            {isExpanded && (
+                <div className={styles.profileDropdown}>
+                    <div className={styles.compactProfile}>
+                        <div className={styles.countryRow}>
+                            <img src="https://flagcdn.com/w20/us.png" width="20" alt="us-flag" />
+                            <span>US</span>
+                        </div>
+                        <div className={styles.statsGrid}>
+                            <div className={styles.stat}>
+                                <span className={styles.statLabel}>Wins</span>
+                                <span className={styles.statValue}>{player.wins || 0}</span>
+                            </div>
+                            <div className={styles.stat}>
+                                <span className={styles.statLabel}>Games</span>
+                                <span className={styles.statValue}>{player.totalGames || 0}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- Client/src/components/Game/GameScreen/Player/PlayersTable.jsx
+++ Client/src/components/Game/GameScreen/Player/PlayersTable.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import PlayerCard from "./PlayerCard";
-import PlayerProfileModal from "../../Modals/ProfileModal";
 import { useUser } from "@contexts/UserContext";
 import { useGameRoom } from "@contexts/GameRoomContext";
 import styles from "./PlayersTable.module.css";
@@ -11,11 +10,6 @@ export default function PlayersTable() {
     const { players } = gameState;
 
     const [selectedPlayer, setSelectedPlayer] = useState(null);
-
-    const handlePlayerCardClick = (player) => {
-        const userData = players.find((p) => p.username === player.username);
-        setSelectedPlayer(userData);
-    };
 
     // Separate players by role
     const keeper = players.find((player) => player.role === "keeper");
@@ -31,7 +25,7 @@ export default function PlayersTable() {
                         <span className={styles.keeperSubtitle}>Defends the secret word</span>
                     </div>
                     <div className={styles.keeperContainer}>
-                        <PlayerCard key={keeper.username} player={keeper} me={keeper.username === user.username} isActiveClueGiver={false} onClick={handlePlayerCardClick} setSelectedPlayer={setSelectedPlayer} />
+                        <PlayerCard key={keeper.username} player={keeper} me={keeper.username === user.username} isActiveClueGiver={false} selectedPlayer={selectedPlayer} setSelectedPlayer={setSelectedPlayer} />
                     </div>
                 </div>
             )}
@@ -49,12 +43,10 @@ export default function PlayersTable() {
                 </div>
                 <div className={styles.seekersContainer}>
                     {seekers.map((player) => (
-                        <PlayerCard key={player.username} player={player} me={player.username === user.username} isActiveClueGiver={player.username === gameState.clueGiverUsername} onClick={handlePlayerCardClick} setSelectedPlayer={setSelectedPlayer} />
+                        <PlayerCard key={player.username} player={player} me={player.username === user.username} isActiveClueGiver={player.username === gameState.clueGiverUsername} selectedPlayer={selectedPlayer} setSelectedPlayer={setSelectedPlayer} />
                     ))}
                 </div>
             </div>
-
-            {selectedPlayer && <PlayerProfileModal selectedPlayer={selectedPlayer} onClose={() => setSelectedPlayer(null)} />}
         </div>
     );
 }
